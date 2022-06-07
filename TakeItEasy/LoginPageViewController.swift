720742c58b2dc58f56f0ac5f20c9c19ceae97753
abarne <celius377@gmail.com>
2022-06-07 17:32:59 -0400

    Login storyboard, LoginPageViewController added

---
+++ TakeItEasy/LoginPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPageViewController.swift
+//  TakeItEasy
+//
+//  Created by admin on 6/7/22.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
