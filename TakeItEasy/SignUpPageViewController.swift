eec4a77631a569bb366a3f5210258c2bce68d707
abarne <celius377@gmail.com>
2022-06-09 00:58:09 -0400

    email/password validation done. basic UI laid out. added check for all text fields filled out correctly.

--- TakeItEasy/SignUpPageViewController.swift
+++ TakeItEasy/SignUpPageViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SignUpPageViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var getOPT: UIButton!
+    @IBOutlet weak var phoneReqText: UILabel!
     @IBOutlet weak var phoneNumberField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var nameField: UITextField!
@@ -20,6 +22,29 @@ class SignUpPageViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nameReqText: UILabel!
     @IBOutlet weak var reEnterPasswordField: UITextField!
     var activeTextField = UITextField()
+
+    
+    func isValidPassword(_ str : String) -> Bool {
+        let password = str
+        let pswdRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&<>*~:`-]).{8,}$"
+        let pswdCheck = NSPredicate(format: "SELF MATCHES %@", pswdRegex)
+        return pswdCheck.evaluate(with: password)
+    }
+    
+    func isValidEmail(_ str : String) -> Bool {
+        let email = str
+        let emailRegex = #".+\@.+\..+"#
+        let emailCheck = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailCheck.evaluate(with: email)
+    }
+    
+    func allFieldsFilledOut() -> Bool {
+        if(nameField.text != "" && isValidEmail(emailField.text!) && phoneNumberField.text != "" && isValidPassword(passwordField.text!) && passwordField.text == reEnterPasswordField.text){
+            return true
+        }else{
+            return false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,41 +65,68 @@ class SignUpPageViewController: UIViewController, UITextFieldDelegate {
         case reEnterPasswordField:
             passwordMatchLabel.isHidden = false
         default:
-            print("none")
+            print("default")
         }
         
     }
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        if textField == reEnterPasswordField{
+        
+        switch textField{
+        case reEnterPasswordField:
             if textField.text != passwordField.text {
                 passwordMatchLabel.textColor = .red
             }else{
                 passwordMatchLabel.textColor = .black
                 passwordMatchLabel.text = "Passwords Match"
             }
+
+        default:
+            print("default")
         }
     }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         switch textField {
         case passwordField:
-            passwordReqText.isHidden = true
+            if(isValidPassword(passwordField.text!)){
+                passwordReqText.textColor = .black
+                passwordReqText.isHidden = true
+            }else{
+                passwordReqText.textColor = .red
+            }
         case nameField:
             if(nameField.text == ""){
-                self.nameReqText.isHidden = false
+                nameReqText.isHidden = false
             }else{
-                self.nameReqText.isHidden = true
+                nameReqText.isHidden = true
             }
         case emailField:
             if(emailField.text?.range(of: #".+\@.+\..+"#, options: .regularExpression) != nil){
-                self.emailReqText.isHidden = true
+                emailReqText.isHidden = true
             }else{
-                self.emailReqText.isHidden = false
+                emailReqText.isHidden = false
+            }
+        case phoneNumberField:
+            if(phoneNumberField.text == ""){
+                phoneReqText.isHidden = false
+            }else{
+                phoneReqText.isHidden = true
             }
         default:
             print("none")
         }
     }
     
-
+    @IBAction func getOPTPressed(_ sender: Any) {
+        if(allFieldsFilledOut()){
+            print("send OPT")
+        }else{
+            let defaultAction = UIAlertAction(title: "Ok", style: .default){(action) in}
+            let alert = UIAlertController(title: "Error", message: "All Fields must be filled out", preferredStyle: .alert)
+            alert.addAction(defaultAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
     
 }
