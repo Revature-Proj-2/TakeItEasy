3e8170c4c27fdb9b285dd65f2b1df5ece78cffe1
abarne <celius377@gmail.com>
2022-06-09 12:49:18 -0400

    login and remember me functionality complete

--- TakeItEasy/Login/LoginPageViewController.swift
+++ TakeItEasy/Login/LoginPageViewController.swift
@@ -9,10 +9,55 @@ import UIKit
 
 class LoginPageViewController: UIViewController {
 
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var rememberMeSwitch: UISwitch!
+    
+    let userDefault = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if userDefault.string(forKey: "takeItEasyRememberMe") == "yes"{
+            rememberMeSwitch.setOn(true, animated: true)
+            userNameField.text = userDefault.string(forKey: "takeItEasyUserName")
+            let req : [String : Any] = [kSecClass as String : kSecClassGenericPassword, kSecAttrAccount as String : userNameField.text!, kSecReturnAttributes as String : true, kSecReturnData as String : true]
 
-        // Do any additional setup after loading the view.
+            var res : CFTypeRef?
+            if SecItemCopyMatching(req as CFDictionary, &res) == noErr{
+                let data = res as? [String : Any]
+                let userPassword = (data![kSecValueData as String] as? Data)!
+                let pass = String(data: userPassword, encoding: .utf8)
+                passwordField.text = pass
+            }
+        }
     }
+    
+    @IBAction func logInButtonPressed(_ sender: Any) {
+        let req : [String : Any] = [kSecClass as String : kSecClassGenericPassword, kSecAttrAccount as String : userNameField.text!, kSecReturnAttributes as String : true, kSecReturnData as String : true]
+        var res : CFTypeRef?
+        if SecItemCopyMatching(req as CFDictionary, &res) == noErr{
+            let data = res as? [String : Any]
+            let userName = (data![kSecAttrAccount as String] as? String)!
+            let userPassword = (data![kSecValueData as String] as? Data)!
+            let pass = String(data: userPassword, encoding: .utf8)
+            if passwordField.text == pass {
+                userDefault.set(userName, forKey: "takeItEasyCurrentLoggedIn")
+                if rememberMeSwitch.isOn {
+                    userDefault.set("yes", forKey: "takeItEasyRememberMe")
+                    userDefault.set(userName, forKey: "takeItEasyUserName")
+                }else{
+                    userDefault.set("no", forKey: "takeItEasyRememberMe")
+                    userDefault.set("", forKey: "takeItEasyUserName")
+                }
+                print("transition to next page here")
+            }
+        }else{
+            let defaultAction = UIAlertAction(title: "Ok", style: .default){(action) in}
+            let alert = UIAlertController(title: "Error", message: "An error has occurred", preferredStyle: .alert)
+            alert.addAction(defaultAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
 
 }
