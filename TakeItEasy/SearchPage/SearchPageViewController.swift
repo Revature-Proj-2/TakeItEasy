aa4177aaaf01fe6194dd523360b4b870d6d38c20
abarne <celius377@gmail.com>
2022-06-10 11:40:38 -0400

    SearchPageViewController added

---
+++ TakeItEasy/SearchPage/SearchPageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchPageViewController.swift
+//  TakeItEasy
+//
+//  Created by admin on 6/10/22.
+//
+
+import UIKit
+import WebKit
+
+class SearchPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
